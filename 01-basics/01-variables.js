c57cc3f4cb93314eff8fec662821568c9defdcb3
Avnish Ranjan <112241781+codingwithranjan@users.noreply.github.com>
2024-07-17 12:03:57 +0000

    container configure

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,6 @@
+const accountId = 838629
+let accountEmail = "ranjan@ranjan.com"
+accountPassword = "52526367"
+accountCity = "biharsharif"
+console.log(accountId);
+console.table([accountId, accountEmail, accountCity, accountPassword])
